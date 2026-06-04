c638059d6acbfa0982da90dcd3f7eebffe6a672c
TCoder4k <tucaqn1@gmail.com>
2026-06-04 21:06:08 +0700

    connection with mysql

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const configViewEngine = require("./config/viewEngine");
+const mysql = require("mysql2");
 
 const webRoutes = require("./routes/web");
 const app = express();
@@ -10,6 +11,26 @@ const hostname = process.env.HOST_NAME;
 
 // config template engine
 configViewEngine(app);
+
+//connection db
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307,
+  user: "root",
+  password: "123456",
+  database: "tcoder4k",
+});
+
+//querry db
+connection.query("SELECT * FROM Users ", function (err, results, fields) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log("results: ", results);
+  console.log("fields: ", fields);
+});
 
 //define route
 app.use("/", webRoutes);
