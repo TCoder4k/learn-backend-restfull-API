37fcc712f5f3a79589474ef85e92fc07d9ad1684
TCoder4k <tucaqn1@gmail.com>
2026-06-07 21:20:05 +0700

    add navbar

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,20 +1,7 @@
 const connection = require("../config/database");
 
 const getHomePage = (req, res) => {
-  //process data
-  //call model
-  let users = [];
-
-  connection.query("SELECT * FROM Users ", function (err, results, fields) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    users = results;
-    console.log("checkUserHomePage: ", results);
-    res.send(JSON.stringify(users));
-    console.log("users:", users);
-  });
+  return res.render("home.ejs");
 };
 const getAboutPage = (req, res) => {
   res.render("sample.ejs");
