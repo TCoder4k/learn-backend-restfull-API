7bce8c3e0860f88ced8099ede3b9e703ce886f37
TCoder4k <tucaqn1@gmail.com>
2026-06-07 20:42:24 +0700

    use connection pool to increase performance

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,20 @@
+const connection = require("../config/database");
+
 const getHomePage = (req, res) => {
   //process data
   //call model
-  res.send("<h1>Hello World! & TCODER4K <h1>");
+  let users = [];
+
+  connection.query("SELECT * FROM Users ", function (err, results, fields) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    users = results;
+    console.log("checkUserHomePage: ", results);
+    res.send(JSON.stringify(users));
+    console.log("users:", users);
+  });
 };
 const getAboutPage = (req, res) => {
   res.render("sample.ejs");
