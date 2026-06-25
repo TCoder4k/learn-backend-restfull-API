01878aee225134a39f377397dbb9af7e950ac48b
TCoder4k <tucaqn1@gmail.com>
2026-06-25 10:52:41 +0700

    using async/await for query db

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -9,23 +9,21 @@ const getAboutPage = (req, res) => {
 const getCreateUsertPage = (req, res) => {
   res.render("createUser.ejs");
 };
-const postCreateUser = (req, res) => {
-  let { email, name, city } = req.body;
-  console.log(">>>Check req.body", "name", name, "email", email, "city", city);
+const postCreateUser = async (req, res) => {
+  try {
+    const { email, name, city } = req.body;
+    console.log(">>>Check req.body", name, email, city);
 
-  //  INSERT INTO Users (email, name , city)
-  // VALUES ('khuonglieu@gmail.com', 'khuong', 'Nghe An');
-  connection.query(
-    "INSERT INTO Users (email, name, city) VALUES (?, ?, ?)",
-    [email, name, city],
-    (err, results) => {
-      if (err) {
-        console.error("Lỗi khi tạo user:", err);
-        return res.status(500).send("Tạo user thất bại!");
-      }
-      res.send(`Tạo user thành công với ID: ${results.insertId}`);
-    },
-  );
+    // destructuring
+    const [results] = await connection.query(
+      "INSERT INTO Users (email, name, city) VALUES (?, ?, ?)",
+      [email, name, city],
+    );
+    res.send(`Tạo user thành công với ID: ${results.insertId}`);
+  } catch (err) {
+    console.error("Lỗi khi tạo user:", err);
+    res.status(500).send("Tạo user thất bại!");
+  }
 };
 
 module.exports = {
